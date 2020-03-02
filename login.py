58b2c0ae4d822eda8a15733c0f78cee0cf8849ab
Gustavo Henrique <gugahribeiro@hotmail.com>
2020-03-02 14:29:38 -0300

    first commit

---
+++ login.py
@@ -0,0 +1,19 @@
+import time, subprocess
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
+
+subprocess.call(['C:\Riot Games\League of Legends\LeagueClient.exe'])
+time.sleep(5) #It depends on how much time your computer takes to open lol 
+
+'''Probably on your first daily open it's gonna take a while, try something about 10sec'''
+
+for index in range(0,2): 
+    time.sleep(1)
+    keyboard.press(Key.tab)   
+
+keyboard.type('') # as String your username here 
+keyboard.press(Key.tab)
+keyboard.type('') #as String Your password here 
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
